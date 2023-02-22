f73a535e2e7c81460215161aafbdd0209b0b72cc
rani12UX <hyeran01027@naver.com>
2023-02-22 04:58:53 +0000

    처음

---
+++ js/nutella.js
@@ -0,0 +1,22 @@
+var main = new Swiper(".main", {
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+});
+var mainslide = new Swiper(".mainslide", {
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+});
+
+var recipy = new Swiper(".recipy", {
+    slidesPerView: 2,
+    spaceBetween: 50,
+    // pagination: {
+    //   el: ".swiper-pagination",
+    //   clickable: true,
+    // },
+    slidesPerGroup: 2,
+  });
